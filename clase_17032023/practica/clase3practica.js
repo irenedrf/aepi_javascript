9ab1b9bc8c31cad2cdd8eef8e75b3b887767a65c
irenedrf <irenedrf@gmail.com>
2023-03-21 20:21:38 +0100

    ejercicios clase 3 partes 1 y 2

--- clase_17032023/practica/clase3practica.js
+++ clase_17032023/practica/clase3practica.js
@@ -1,5 +1,5 @@
 //práctica clase 3 parte 1
-/*
+
 //1
 
 //a
@@ -134,9 +134,63 @@ for (let i of got) {
     console.log(`Soy ${i.nombre} ${i.apellido} de la ciudad ${i.ciudad}`);
 
 };
-*/
 
-//continuar con eejercicios dle 6 en adelantee
+
+
+
+//6 NO CONSEGUIDO
+let myArray = [200, 40, 400, 40, 40];
+
+
+
+
+
+
+
+//7
+let dimeHastaDondeCuento = parseInt(prompt('Hasta qué número qiueres que cuente'));
+let cadena = [];
+for (i = 0; i <= dimeHastaDondeCuento; i++) {
+    cadena.push(i);
+}
+console.log(cadena);
+alert(cadena.join(','));
+
+//8
+let dimeDesdeDondeCuento = parseInt(prompt('Hasta qué número qiueres que cuente hacia atrás'));
+let cadena2 = [];
+for (i = dimeDesdeDondeCuento; i > 0; i--) {
+    cadena2.push(dimeDesdeDondeCuento);
+    dimeDesdeDondeCuento--;
+}
+console.log(cadena2);
+alert(cadena2.join(','));
+
+
+
+//9
+let dimeUnNumero = parseInt(prompt('Dime un número y sumaré los enteros positivos hasta él'));
+
+let suma1 = 0;
+for (i = 0; i <= dimeUnNumero; i++) {
+    suma += i;
+}
+alert(`Los enteros del 0 a ${dimeUnNumero} suman ${suma1}`);
+
+
+
+//10
+let numeroADividir = parseInt(prompt('Dime un número y te digo si al dividirlo por 3 da 2'));
+
+let division = 0;
+let arrayDivisibles = [];
+
+for (i = 0; i < numeroADividir; i++) {
+    if (i % 3 == 2) {
+        arrayDivisibles.push(i);
+    }
+}
+console.log(arrayDivisibles);
 
 
 //ejercicios clase 3 parte 2
@@ -193,3 +247,127 @@ function contarVocales(cadena) {
     return cantidadVocales;
 }
 console.log(contarVocales('EsternoCleidoMastoIdeo'));
+
+
+//4
+function sumaPares(numeroPositivo) {
+    let sum = 0;
+    for (i = 2; i <= numeroPositivo; i++) {
+        if (i % 2 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(sumaPares(10));
+
+
+
+//5
+function factorial(numero) {
+    let prod = 1;
+    for (i = 1; i <= numero; i++) {
+        console.log(i);
+        prod *= i;
+
+    }
+    return prod;
+}
+
+console.log(factorial(5));
+
+
+
+//6
+function numeroEsPar(numero) {
+    let esPar = false;
+    if (numero % 2 == 0) {
+        esPar = true;
+    }
+
+    mostrarResultado(esPar);
+}
+
+function mostrarResultado(resultado) {
+    console.log(resultado);
+}
+
+numeroEsPar(21);
+
+//7
+
+function calcularPromedio(arrayCalificaciones) {
+    let sumaTotal = 0;
+    for (calificacion of arrayCalificaciones) {
+
+        sumaTotal += calificacion;
+
+    }
+    let promedio = sumaTotal / arrayCalificaciones.length;
+
+    mostrarPromedio(promedio);
+}
+
+function mostrarPromedio(promedio) {
+    console.log('El promedio es ' + promedio);
+}
+
+let calificaciones = [1, 5, 8, 3, 5, 9, 6, 8, 10];
+
+calcularPromedio(calificaciones);
+
+
+//8
+
+function concatenarYOrdenar(arrayUno, arrayDos) {
+    let arrayUnico = [...arrayUno, ...arrayDos];
+    let arrayOrdenado = arrayUnico.sort();
+    mostrarResultadoArrays(arrayOrdenado);
+}
+
+function mostrarResultadoArrays(array) {
+    console.log(array)
+};
+
+let unArray = [7, 9, 2];
+let dosArray = [4, 1, 8, 3];
+//concatenarYOrdenar(unArray, dosArray);
+
+
+
+//9
+function numeroMasGrande(arrayNumeros) {
+    let ordenarArray = arrayNumeros.sort();
+    console.log(ordenarArray);
+    let numeroMayor = ordenarArray[ordenarArray.length - 1];
+    console.log(numeroMayor);
+    mostrarResultadoNumero(numeroMayor);
+}
+
+function mostrarResultadoNumero(numeroMayor) {
+    console.log(numeroMayor)
+};
+
+numeroMasGrande(dosArray);
+
+
+
+//10
+function filtrarPalabras(arrayPalabras, longitudMinima) {
+    let nuevoArray = [];
+    for (palabra of arrayPalabras) {
+        if (palabra.length >= longitudMinima) {
+            nuevoArray.push(palabra);
+        }
+    }
+    mostrarResultadoPalabras(nuevoArray);
+}
+
+function mostrarResultadoPalabras(arrayPajarosFiltrados) {
+    console.log(arrayPajarosFiltrados);
+};
+
+let arrayPajaros = ['Pato', 'Avestruz', 'Gallo', 'Pollo', 'Gavilán', 'Águila'];
+
+filtrarPalabras(arrayPajaros, 6);
