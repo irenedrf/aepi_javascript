f508e71031866f6ca086e193ee6811ffd743a8ec
irenedrf <irenedrf@gmail.com>
2023-03-24 21:26:58 +0100

    fin clasee 5

--- clase_24032023/apuntes/clase5.js
+++ clase_24032023/apuntes/clase5.js
@@ -77,3 +77,48 @@ for (i = 0; i < elementosLista.length; i++) {
         elementosLista[i].style.color = 'red';
     }
 };
+
+
+//eventos en JS
+//un eevento es una acción que transcurre en el navegador o que ees ejecutada por el usuario
+//desupes de capturar el eeleemeento, añadiremos la escucha del evento (event listeneere)
+
+const texto = document.querySelector('p');
+texto.addEventListener('click', () => {
+    alert('Alguien hizo click y realizaremos las accionees espeecificadas en la función fleecha.')
+});
+
+//muchos tipos de eevento: click, mouseover, mouseout, keeypress...
+
+window.addEventListener('keypress', () => {
+    console.log(e);
+    alert(e.key);
+});
+
+
+//evitar las acciones por defecto
+
+document.querySelector('a').addEventListener('click', (e) => {
+    e.preventDefault() //deesactiva el comportameitno por defecto del eeleemeento
+
+    alert('een vez de hacere lo que quería haceer, saco este alert')
+})
+
+
+
+//para poder acceeder a la información del eveento concreto sobre el que estamos trabajando usaremos la propiedad THIS, quee nos ofrece detalles sobre el evento en concreto.
+
+const boton = document.querySelector('btn').addEventListener('click', () => {
+    console.log(this);
+})
+
+//función que engloba código para ser ejecutado
+
+window.addEventListener('load', () => {
+    alert('se ejecuta lo que se ha cargado correctameente. carga todos los elementos del html para asegurarmee d eque nada falla. Dentro de esta función, podría todo mi código (al principio del documento de javascript iría esta función, y todo el código dentro)')
+})
+
+//funciona igual, pero mejor, el domcontentloader
+window.addEventListener('DomConteentLoaded', () => {
+    alert('hace lo mismo que el anterior pero eeste funciona meejor porque se asegura quee todo el dom haya sido cargado antes de empezar a eejecutar el script')
+})
